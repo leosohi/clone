e37fc8901025973cdb83856d229d3742c9a45d7f
Master Leo <leosohihi@gmail.com>
2024-03-03 09:35:49 +0800

    final

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import {  HashRouter } from "react-router-dom";
 import { ShopContextProvider } from "./Context/CreateContext";
 import "./Styles/styles.css";
 
@@ -10,8 +10,8 @@ import "./Styles/styles.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ShopContextProvider>
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
   </ShopContextProvider>
 );
